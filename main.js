92289c35ea0ac10f80f6e1d9f14076645c958897
JulianCarpintero04 <juliancarpintero04@gmail.com>
2022-08-18 09:19:03 -0300

    Maquetando

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const usuario = {
-     Nombre: 'Jorge',
-     Apellido: 'Santana'
-};
-
-console.log(JSON.stringify(usuario));
+"use strict";
+let a = 42;
+let b = "Hola";
+let c = 101;
+//c="one";
+let recursos = ['memoria', 'disco', 'procesador', 7];
+// Ahi combinamos strings con numeros
+let partes = ['memoria', 'disco'];
+// Solo acepta strings, no otro tipo de dato
+let message = 'Hello world';
+console.log(message);
